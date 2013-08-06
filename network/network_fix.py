d9b940842a05283a82c1bc667e2b3ec3173d6c33
Matt Parlette <matthew.parlette@gmail.com>
2013-08-06 09:25:43 -0400

    Implemented diagnose_network() to verify a correct IP address.
    This is windows-specific, and will call ipconfig release/renew three times before it gives up.

--- network/network_fix.py
+++ network/network_fix.py
@@ -3,6 +3,14 @@
 import argparse
 import logging
 import os
+import subprocess
+import netifaces as ni
+from netaddr import IPNetwork,IPAddress
+
+log = None
+blacklist_range = [IPNetwork('10.0.0.0/8')]
+whitelist_range = [IPNetwork('192.168.1.0/24')]
+max_attempts = 3
 
 def main():
   parser = argparse.ArgumentParser(description='Resolve IP-related network issues at CLS.')
@@ -12,6 +20,7 @@ def main():
   args = parser.parse_args()
   
   #Setup logger
+  global log
   log = logging.getLogger('network_fix')
   log.setLevel(logging.DEBUG) if args.debug else log.setLevel(logging.INFO)
   
@@ -32,6 +41,47 @@ def main():
   log.addHandler(ch)
   
   log.debug("Logger initialized")
+  
+  keep_trying = True
+  attempts = 0
+  
+  while keep_trying and attempts < max_attempts:
+    if diagnose_network():
+      log.info("All IP Addresses determined to be valid.")
+      keep_trying = False
+    else:
+      log.error("One or more invalid IP addresses were found")
+      
+      #try for a new ip address
+      log.info("Releasing current IP Address...")
+      os.popen("ipconfig /release")
+      log.info("Requesting a new IP Address...")
+      os.popen("ipconfig /renew")
+      log.debug("ipconfig returned")
+    attempts += 1
+  
+  log.debug("Script exiting")
+
+def diagnose_network():
+  """Get all IP addresses assigned to this system.
+  
+  If an IP is in the blacklist, return False."""
+  
+  interfaces = ni.interfaces()
+  for nif in interfaces:
+    log.debug("Processing interface %s" % nif)
+    log.debug("ifaddresses output: %s" % ni.ifaddresses(nif))
+    log.debug("ifaddresses keys: %s" % ni.ifaddresses(nif).keys())
+    if ni.AF_INET in ni.ifaddresses(nif):
+      for addr in ni.ifaddresses(nif)[ni.AF_INET]:
+        log.debug("Read IP of %s" % addr['addr'])
+        for black_addr in blacklist_range:
+          ip = IPAddress(addr['addr'])
+          if ip in black_addr:
+            log.error("IP Address %s found to be in blacklist range %s" % (ip,black_addr))
+            return False
+  
+  return True
 
 if __name__ == '__main__':
   main()
